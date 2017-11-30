e049b23b6c89fcbf5c81a1d29f334951ef69aa3e
Tiago Alves <tralves@gmail.com>
2017-11-30 01:27:26 -0200

    Fix removing file.

--- postinstall.js
+++ postinstall.js
@@ -27,6 +27,9 @@ function log(err) {
 }
 
 function symlinkFromTns(file, type) {
+  if (fs.existsSync(path.join(__dirname, '..', '..', 'tns', file))) {
+    fs.unlinkSync(path.join(__dirname, '..', '..', 'tns', file));
+  }
   fs.symlinkSync(
     path.join(__dirname, '..', '..', 'tns', file),
     path.join(__dirname, '..', '..', file),
