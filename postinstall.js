0d037ea196396efe90a1f37a409fff578329d185
Tiago Alves <tralves@gmail.com>
2017-11-30 01:20:51 -0200

    Remove files to be symlinked.

--- postinstall.js
+++ postinstall.js
@@ -28,8 +28,8 @@ function log(err) {
 
 function symlinkFromTns(file, type) {
   fs.symlinkSync(
-    path.join(__dirname, '..', '..', file),
     path.join(__dirname, '..', '..', 'tns', file),
+    path.join(__dirname, '..', '..', file),
     type);
 }
 
