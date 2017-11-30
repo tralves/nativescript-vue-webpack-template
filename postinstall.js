2871d0135453db19901d588c2815c6f6de2eb790
Tiago Alves <tralves@gmail.com>
2017-11-30 00:33:42 -0200

    Set tns folder assets and files as symlinks.

--- postinstall.js
+++ postinstall.js
@@ -4,6 +4,11 @@ const path = require('path');
 move(getMovePaths('webpack.config.js'), log);
 move(getMovePaths('.babelrc'), log);
 move(getMovePaths('tns'), log);
+symlinkFromTns('package.json', 'file');
+symlinkFromTns('package-lock.json', 'file');
+symlinkFromTns('node-modules', 'dir');
+symlinkFromTns('app/App_Resources', 'dir');
+symlinkFromTns('app/images', 'dir');
 
 fs.unlinkSync('postinstall.js');
 
@@ -18,6 +23,13 @@ function log(err) {
   if (err) {
     console.log(err);
   }
+}
+
+function symlinkFromTns(file, type) {
+  fs.symlinkSync(
+    path.join(__dirname, '..', '..', 'tns', file),
+    path.join(__dirname, '..', '..', file),
+    type);
 }
 
 // move function copied from: https://stackoverflow.com/questions/8579055/how-i-move-files-on-node-js
