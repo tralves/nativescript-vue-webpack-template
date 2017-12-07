f39c2f6540e8afeec387f01f6ff9c0ec5e4ece75
Tiago Alves <tralves@gmail.com>
2017-12-06 23:19:23 -0200

    Revert most of today's work...

--- postinstall.js
+++ postinstall.js
@@ -1,25 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-const spawnSync = require('child_process').spawnSync;
-const symlinkOrCopySync = symlinkOrCopy().sync;
 
 move(getMovePaths('webpack.config.js'), log);
 move(getMovePaths('.babelrc'), log);
 move(getMovePaths('tns'), log);
 
-console.log('listing tns files');
-fs.readdirSync(path.join(__dirname, '..', '..','tns')).forEach(file => {
-  console.log(file);
-})
-
-console.log("running npm install on " + path.join(__dirname, '..', '..'));
-const npmInstall = spawnSync('npm', ['install'], {
-  cwd: path.join(__dirname, '..', '..'),
-  stdio: 'inherit'
-});
-
-console.log('listing app files');
-fs.readdirSync(path.join(__dirname, '..', '..','app')).forEach(file => {
+console.log('listing files');
+fs.readdirSync(path.join(__dirname, '..', '..', 'tns')).forEach(file => {
   console.log(file);
 })
 
@@ -50,7 +37,7 @@ function symlinkFromTns(file, type) {
   if (fs.existsSync(path.join(__dirname, '..', '..', 'tns', file))) {
     fs.unlinkSync(path.join(__dirname, '..', '..', 'tns', file));
   }
-  symlinkOrCopySync(
+  fs.symlinkSync(
     path.join(__dirname, '..', '..', file),
     path.join(__dirname, '..', '..', 'tns', file),
     type);
@@ -58,7 +45,6 @@ function symlinkFromTns(file, type) {
 
 // move function copied from: https://stackoverflow.com/questions/8579055/how-i-move-files-on-node-js
 function move({ oldPath, newPath }, callback) {
-  console.log("moving: " + oldPath + " -> " + newPath);
   fs.rename(oldPath, newPath, function (err) {
     if (err) {
       if (err.code === 'EXDEV') {
@@ -83,136 +69,5 @@ function move({ oldPath, newPath }, callback) {
     });
 
     readStream.pipe(writeStream);
-  }
-}
-
-
-function symlinkOrCopy() {
-  var fs = require('fs')
-  var tmpdir = require('os').tmpdir();
-  var path = require('path')
-
-  var isWindows = process.platform === 'win32'
-  // These can be overridden for testing
-  var options = {
-    isWindows: isWindows,
-    canSymlink: testCanSymlink(),
-    fs: fs
-  }
-
-  function testCanSymlink() {
-    // We can't use options here because this function gets called before
-    // its defined
-    if (isWindows === false) { return true; }
-
-    var canLinkSrc = path.join(tmpdir, "canLinkSrc.tmp")
-    var canLinkDest = path.join(tmpdir, "canLinkDest.tmp")
-
-    try {
-      fs.writeFileSync(canLinkSrc, '');
-    } catch (e) {
-      return false
-    }
-
-    try {
-      fs.symlinkSync(canLinkSrc, canLinkDest)
-    } catch (e) {
-      fs.unlinkSync(canLinkSrc)
-      return false
-    }
-
-    fs.unlinkSync(canLinkSrc)
-    fs.unlinkSync(canLinkDest)
-
-    return true
-  }
-
-  function setOptions(newOptions) {
-    options = newOptions
-  }
-
-  function cleanup(path) {
-    if (typeof path !== 'string') { return }
-    // WSL (Windows Subsystem Linux) has issues with:
-    //  * https://github.com/ember-cli/ember-cli/issues/6338
-    //  * trailing `/` on symlinked directories
-    //  * extra/duplicate `/` mid-path
-    //  issue: https://github.com/Microsoft/BashOnWindows/issues/1421
-    return path.replace(/\/$/, '').replace(/\/\//g, '/');
-  }
-
-  function symlinkOrCopySync(srcPath, destPath) {
-    if (options.isWindows) {
-      symlinkWindows(srcPath, destPath)
-    } else {
-      symlink(srcPath, destPath)
-    }
-  }
-
-  Object.defineProperty(module.exports, 'canSymlink', {
-    get: function () {
-      return !!options.canSymlink;
-    }
-  });
-
-  function symlink(_srcPath, _destPath) {
-    var srcPath = cleanup(_srcPath);
-    var destPath = cleanup(_destPath);
-
-    var lstat = options.fs.lstatSync(srcPath)
-    if (lstat.isSymbolicLink()) {
-      // When we encounter symlinks, follow them. This prevents indirection
-      // from growing out of control.
-      // Note: At the moment `realpathSync` on Node is 70x slower than native,
-      // because it doesn't use the standard library's `realpath`:
-      // https://github.com/joyent/node/issues/7902
-      // Can someone please send a patch to Node? :)
-      srcPath = options.fs.realpathSync(srcPath)
-    } else if (srcPath[0] !== '/') {
-      // Resolve relative paths.
-      // Note: On Mac and Linux (unlike Windows), process.cwd() never contains
-      // symlink components, due to the way getcwd is implemented. As a
-      // result, it's correct to use naive string concatenation in this code
-      // path instead of the slower path.resolve(). (It seems unnecessary in
-      // principle that path.resolve() is slower. Does anybody want to send a
-      // patch to Node?)
-      srcPath = process.cwd() + '/' + srcPath
-    }
-    options.fs.symlinkSync(srcPath, destPath);
-  }
-
-  // Instruct Win32 to suspend path parsing by prefixing the path with a \\?\.
-  // Fix for https://github.com/broccolijs/broccoli-merge-trees/issues/42
-  var WINDOWS_PREFIX = "\\\\?\\";
-
-  function symlinkWindows(srcPath, destPath) {
-    var stat = options.fs.lstatSync(srcPath)
-    var isDir = stat.isDirectory()
-    var wasResolved = false;
-
-    if (stat.isSymbolicLink()) {
-      srcPath = options.fs.realpathSync(srcPath);
-      isDir = options.fs.lstatSync(srcPath).isDirectory();
-      wasResolved = true;
-    }
-
-    srcPath = WINDOWS_PREFIX + (wasResolved ? srcPath : path.resolve(srcPath));
-    destPath = WINDOWS_PREFIX + path.resolve(path.normalize(destPath));
-
-    if (options.canSymlink) {
-      options.fs.symlinkSync(srcPath, destPath, isDir ? 'dir' : 'file');
-    } else {
-      if (isDir) {
-        options.fs.symlinkSync(srcPath, destPath, 'junction');
-      } else {
-        options.fs.writeFileSync(destPath, options.fs.readFileSync(srcPath), { flag: 'wx', mode: stat.mode })
-        options.fs.utimesSync(destPath, stat.atime, stat.mtime)
-      }
-    }
-  }
-
-  return {
-    sync: symlinkOrCopySync,
-    setOptions: setOptions
   }
 }
